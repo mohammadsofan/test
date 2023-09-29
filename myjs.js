ab3d8aabb2fc4a3a39fb1c6ac94bc7effe75521e
mohammadsofan <146251718+mohammadsofan@users.noreply.github.com>
2023-09-29 12:48:17 +0300

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,51 @@
+
+// document.getElementById("colors").style.transform = "translate(100%, 0)";
+// window.onload = function () {
+//     var d = document.getElementById("div");
+//     d.style.backgroundColor = localStorage.getItem("color");
+
+// }
+
+
+
+// function chColor(e) {
+//     var d = document.getElementById("div");
+//     d.style.backgroundColor = e.style.color;
+//     localStorage.setItem("color", e.style.color);
+// }
+
+
+// document.getElementById("options").onclick = function () {
+
+//     var c = document.getElementById("colors");
+//     if (c.getAttribute("hide") == "true") {
+//         c.style.transition = "0.4s all";
+//         c.style.transform = "translate(0, 0)";
+//         c.setAttribute("hide", "false");
+//     }
+//     else {
+//         c.style.transition = "0.4s all";
+//         c.style.transform = "translate(100%, 0)";
+//         c.setAttribute("hide", "true");
+//     }
+
+// }
+
+// var d = document.getElementsByTagName("li");
+
+// function fun() {
+//     for (let i = 0; i < d.length; i++) {
+//         d[i].classList.remove("active");
+//     }
+//     this.classList.add("active");
+
+// }
+
+// for (let i = 0; i < d.length; i++) {
+//     d[i].addEventListener("click", fun);
+// }
+
+
+
+
+
